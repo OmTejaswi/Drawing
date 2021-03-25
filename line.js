1853eb4098017b1a8c51b938a38b51b4631c99b1
Om Tejaswi <omtejaswi2008classes@gmail.com>
2021-03-25 18:01:50 +0530

    updated

--- line.js
+++ line.js
@@ -1,15 +1,41 @@
-class lines {
-    constructor(color){
+class shape {
+    constructor(color,select,shape){
         this.cx = mouseX;
         this.cy = mouseY;
 
         this.px = pwinMouseX;
         this.py = pwinMouseY;
 
-        this.color = color
+        this.color = color;
+        this.select = select;
+
+        this.shape = shape;
     }
     display(){
         stroke(this.color);
-        line(this.px,this.py,this.cx,this.cy);
+        strokeWeight(this.select);
+        if(this.shape === "Line") {
+            line(this.px,this.py,this.cx,this.cy);
+        }
+        if(this.shape === "Circle") {
+            noStroke();
+            ellipse(this.cx,this.cy,this.select*6)
+        }
+        if(this.shape === "Square") {
+            noStroke();
+            rectMode(CENTER);
+            rect(this.cx,this.cy,this.select*6,this.select*6)
+        }
+        if(this.shape === "Rectangle") {
+            noStroke();
+            rectMode(CENTER);
+            rect(this.cx,this.cy,this.select*10,this.select*4)
+        }
+        if(this.shape === "Line") {
+            line(this.px,this.py,this.cx,this.cy);
+        }
+        if(this.shape === "Line") {
+            line(this.px,this.py,this.cx,this.cy);
+        }
     }
 }
--- sketch.js
+++ sketch.js
@@ -1,18 +1,38 @@
 var lin, liArr = []
 var colorPick,colorPen;
-
+var selector;
+var shapeSelector;
 function setup() {
     createCanvas(windowWidth,windowHeight);
 
-    colorPick = createColorPicker("fff");
-    colorPen = createColorPicker().position(200,100)
+    colorPick = createColorPicker("fff").position(width/30,height/1.07);
+    colorPen = createColorPicker().position(colorPick.x+70,height/1.07);
+    selector = createSelect().position(colorPen.x+70,height/1.07);
+    selector.option("1");
+    selector.option("2");
+    selector.option("3");
+    selector.option("4");
+    selector.option("5");
+    selector.option("6");
+    selector.option("7");
+    selector.option("8");
+    selector.option("9");
+    selector.option("10");
+
+    shapeSelector = createSelect().position(selector.x+70,height/1.07);
+    shapeSelector.option("Line");
+    shapeSelector.option("Circle");
+    shapeSelector.option("Square");
+    shapeSelector.option("Rectangle");
+    shapeSelector.option("Triangle");
+    shapeSelector.option("Star");
 }
 
 function draw() {
     background(colorPick.value());
     
     if(mouseIsPressed) {
-        lin = new lines(colorPen.value());
+        lin = new shape(colorPen.value(),eval(selector.value()),shapeSelector.value());
         liArr.push(lin);
     }
     for(var i in liArr) {
