4991430b99f41109295611a05dc510fdf1c23876
Om Tejaswi <omtejaswi2008classes@gmail.com>
2021-03-26 17:41:28 +0530

    updated

--- line.js
+++ line.js
@@ -1,5 +1,5 @@
 class shape {
-    constructor(color,select,shape){
+    constructor(color,size,shape){
         this.cx = mouseX;
         this.cy = mouseY;
 
@@ -7,35 +7,59 @@ class shape {
         this.py = pwinMouseY;
 
         this.color = color;
-        this.select = select;
+        this.size = size;
 
         this.shape = shape;
     }
     display(){
         stroke(this.color);
-        strokeWeight(this.select);
+        strokeWeight(this.size);
         if(this.shape === "Line") {
             line(this.px,this.py,this.cx,this.cy);
         }
         if(this.shape === "Circle") {
             noStroke();
-            ellipse(this.cx,this.cy,this.select*6)
+            ellipse(this.cx,this.cy,this.size*6)
         }
         if(this.shape === "Square") {
             noStroke();
             rectMode(CENTER);
-            rect(this.cx,this.cy,this.select*6,this.select*6)
+            rect(this.cx,this.cy,this.size*6,this.size*6)
         }
         if(this.shape === "Rectangle") {
             noStroke();
             rectMode(CENTER);
-            rect(this.cx,this.cy,this.select*10,this.select*4)
+            rect(this.cx,this.cy,this.size*10,this.size*4)
         }
-        if(this.shape === "Line") {
-            line(this.px,this.py,this.cx,this.cy);
+        if(this.shape === "Triangle") {
+            noStroke();
+            push();
+            translate(this.cx,this.cy);
+            beginShape();
+            for(let i =0; i < 360; i+=120) {
+                    var x,y;
+                    x = this.size*sin(i)*10;
+                    y = this.size*cos(i)*10;
+
+                    vertex(x,y);
+            }
+            endShape(CLOSE);
+            pop();
         }
-        if(this.shape === "Line") {
-            line(this.px,this.py,this.cx,this.cy);
+        if(this.shape === "Star") {
+            noStroke();
+            push();
+            translate(this.cx,this.cy);
+            beginShape();
+            for(let i =0; i < 720; i+=144) {
+                    var x,y;
+                    x = this.size*sin(i)*10;
+                    y = this.size*cos(i)*10;
+
+                    vertex(x,y);
+            }
+            endShape(CLOSE);
+            pop();
         }
     }
 }
